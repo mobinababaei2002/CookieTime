70e35701b6716780455e7d9bd9d8d969e573d635
mobinababaei2002 <134775973+mobinababaei2002@users.noreply.github.com>
2026-01-08 14:01:34 +0330

    Update script.js

--- script/script.js
+++ script/script.js
@@ -180,4 +180,15 @@ document.addEventListener("DOMContentLoaded", () => {
           });
         }
       
+
       });
+document.addEventListener("DOMContentLoaded", () => {
+    const toggle = document.querySelector(".menu-toggle");
+    const nav = document.querySelector("nav");
+
+    if (!toggle || !nav) return;
+
+    toggle.addEventListener("click", () => {
+        nav.classList.toggle("active");
+    });
+});
